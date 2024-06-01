965ca2eb94ceed1ed9d8fe1efdafe183169ceb9e
Rakhmanin Aleksandr <a.rachmanin@yandex.ru>
2024-06-01 15:33:13 +0600

    Закончил писать контрольную, добавил оставшиеся файлы index и файлы ts

---
+++ src/types.d.ts
@@ -0,0 +1,18 @@
+export type FoodConst = {
+    id:string;
+    name:string;
+    price:number;
+    image:img;
+}
+
+export type OrderType = {
+    name: string;
+    price: number;
+    image: string;
+    id: string;
+}
+
+export type Count ={
+    name: string;
+    count: number;
+}
